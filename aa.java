d810efbea8a778ee11c77c6ec92a018b330690df
MGayathiri <gayathiricse14@gmail.com>
2016-08-09 17:17:55 +0530

    Update aa.java

--- aa.java
+++ aa.java
@@ -2,6 +2,11 @@ import java.io.*;
 import java.util.*;
 class aa{
 public static void main(String[] args){
-System.out.println("haiiii...");
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+  if(n>0)
+     System.out.println(n%100);
+else
+  System.out.println(-1);
 }
 }
