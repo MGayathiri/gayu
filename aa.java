756e5fc6f292a910ee7279fdfa4d0ed67561590b
MGayathiri <gayathiricse14@gmail.com>
2016-08-09 18:58:41 +0530

    Update aa.java

--- aa.java
+++ aa.java
@@ -4,9 +4,11 @@ class aa{
 public static void main(String[] args){
 Scanner s=new Scanner(System.in);
 int n=s.nextInt();
-  if(n>0)
-     System.out.println(1);
-else
-  System.out.println(-1);
+if(n%2!=0)
+{System.out.println("odd");}
+else{
+  System.out.println("even");
+}
+  
 }
 }
