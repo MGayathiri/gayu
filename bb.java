aca14f68703a2f70750609c7050debb6712e9aeb
MGayathiri <gayathiricse14@gmail.com>
2016-08-13 13:31:19 +0530

    Create bb.java

---
+++ bb.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+class b{
+public static void main(String[] args){
+Scanner sc=Scanner(System.in);
+System.out.println("enter the no of stirngs");
+itn n=sc.nextInt();
+if(n>0)
+{
+System.out.println("enter the strings");
+String s[]=new String[n];
+for(int i=0;i<n;i++){
+s[i]=new String();
+s[i]=sc.next();
+}
+int max=0;p=0;
+//-------
+for(int i=0;i<n-1;i++){
+for(int k=i;k<n;k++){
+int c=0;
+//---
+for(int j=0;j<s[i].length();j++){
+if(j<s[k].length){
+if(s[i].charAt(j)==s[k].charAt(j)){
+c++;
+}
+else{break;}
+}
+else{break;}
+}
+if(c>m){p=i;}
+//----
+}
+}
+//--------
+System.out.println(s[p].substring(0,m));
+}
+else{
+System.out.println("null");
+}
+}
+}
