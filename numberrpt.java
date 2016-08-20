8b5c16fd5037e6cc476a4dcca0af891dfd096366
MGayathiri <gayathiricse14@gmail.com>
2016-08-20 09:59:07 +0530

    Update numberrpt.java

--- numberrpt.java
+++ numberrpt.java
@@ -4,7 +4,7 @@ class numberrpt{
 public static void main(String[] args){
 Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
-if(n>2){
+if(n>1){
 int a[]=new int[n];  
 for(int i=0;i<n;i++){
   a[i]=sc.nextInt();
