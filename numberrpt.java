8aa9a3bcff1a0005695ea3409f0bf8ddc90b7321
MGayathiri <gayathiricse14@gmail.com>
2016-08-20 09:56:57 +0530

    Create numberrpt.java

---
+++ numberrpt.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class numberrpt{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n>2){
+int a[]=new int[n];  
+for(int i=0;i<n;i++){
+  a[i]=sc.nextInt();
+  }
+for(int i=0;i<n;i++){
+if(a[i]!=0){
+  int x=a[i],c=0;
+  for(int j=0;j<n;j++){
+    if(x==a[j]){c++;}
+    }
+  if(c!=0&&c<2){
+  System.out.println(x);
+  }
+  }
+ }
+ }
+}
+}
