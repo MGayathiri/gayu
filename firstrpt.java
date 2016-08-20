1440ddff94e3aef580ede27a58e78c8fbd1d8131
MGayathiri <gayathiricse14@gmail.com>
2016-08-20 11:21:43 +0530

    Update firstrpt.java

--- firstrpt.java
+++ firstrpt.java
@@ -2,6 +2,24 @@ import java.io.*;
 import java.util.*:
 class firstrpt{
 public static void main(String[] args){
+  Scanner sc=new Scanne(System.in);
+  int n=sc.nextInt();
+  if(n>1){
+  int a[]=new int[n];
+  for(int i=0;i<n;i++){
+    a[i]=sc.nextInt();
+  }  
+  for(int i=0;i<n;i++){
+  if(x!=0){
+    int c=0,x=a[i]; 
+    for(int j=0;j<n;j++){
+    if(x==a[j]){c++;a[j]=0;}
+    }
+    if(c>1){System.out.println(x);break;}
+   }
+  }
+    
+  }
 
 }
 }
