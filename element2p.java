7166f99c24e378b9979f4c31f2ecd14076442ed6
MGayathiri <gayathiricse14@gmail.com>
2016-08-22 08:59:50 +0530

    Create element2p.java

---
+++ element2p.java
@@ -0,0 +1,32 @@
+import java.io.*:
+import java.util.*;
+class element2p{
+public static void main(String[] args){
+Scanner sc=new Scanner(Sytem.in);
+int n=sc.nextInt();
+int a[]=new int [n];
+if(n>0){
+for(int i=0;i<n;i++){
+ a[i]=sc.nextInt();
+ }
+//-----------
+for(int i=0;i<n;i++){
+ if(i*2<n){
+ for(int j=0;j<n;j++){
+  if(j*2<n){
+  for(int k=0;k<n;k++){
+   if(k*2<n){
+   if(a[2*i]+a[2*j]==a[k*2]){
+    System.out.println("2i="+a[i*2]+" 2j="+a[j*2]+" 2k="+a[k*2]);
+      }  
+     }
+    }
+   }
+  }
+ }
+}
+
+
+}
+}
+}
