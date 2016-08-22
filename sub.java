2d505c6085ea1243d59c7e47b401bcadd7d5bbb7
MGayathiri <gayathiricse14@gmail.com>
2016-08-22 11:05:01 +0530

    Update sub.java

--- sub.java
+++ sub.java
@@ -4,5 +4,8 @@ class sub{
 public static void main(String[] args){
 Scanner sc=new Scanner();
 String s=sc.next();
+for(int i=0;i<s.length();i++){
+  System.out.println(s.substring(0,i)+s.ubstring(i,i+n));
+  }
 }
 }
