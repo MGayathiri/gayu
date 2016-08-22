8174a3f7878e817732fa28497d9d856a5a1c0e5a
MGayathiri <gayathiricse14@gmail.com>
2016-08-22 19:02:20 +0530

    Update heightofnth.java

--- heightofnth.java
+++ heightofnth.java
@@ -3,17 +3,15 @@ import java.util.*;
 class heightofnth{
 public static void main(String[] args){
 Scanner sc=new Scanner(System.in);
-int n=sc.nextInt();
-if(n>5){
-int a[]=new int [n];
-for(int i=0;i<n;i++){
-a[i]=sc.nextInt();}
+double a[]=new double [50];
+for(int i=0;i<50;i++){
+a[i]=sc.nextDouble();}
 Arrays.sort(a);
 int x=sc.nextInt();
 if(x<n){
-System.out.println("4th"+a[a.length-5]);
-System.out.println("nth"+a[x]);
+System.out.println("4th"+a[a.length-4]);
+System.out.println("nth"+a[x-1]);
  }
- }
+ 
 } 
 }
