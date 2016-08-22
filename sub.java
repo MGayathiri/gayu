cddf07a50d1c47fe861d0b8d52dc0f74566e5646
MGayathiri <gayathiricse14@gmail.com>
2016-08-22 11:15:00 +0530

    Update sub.java

--- sub.java
+++ sub.java
@@ -2,10 +2,11 @@ import java.io.*;
 import java.util.*;
 class sub{
 public static void main(String[] args){
-Scanner sc=new Scanner();
+Scanner sc=new Scanner(System.in);
 String s=sc.next();
 for(int i=0;i<s.length();i++){
-  System.out.println(s.substring(0,i)+s.ubstring(i,i+n));
+  for(int j=i+1;j<=s.length();j++){
+  System.out.println(s.substring(i,j));}
   }
 }
 }
