ce113db452fe39496c6e4d14c888b40cd013a71d
MGayathiri <gayathiricse14@gmail.com>
2016-08-23 10:42:41 +0530

    Update recurrever.java

--- recurrever.java
+++ recurrever.java
@@ -3,6 +3,7 @@ import jva.util.*:
 class recurrever{
 public static void main(String[] args){
 Scanner sc=new Scanner(System.in);
+static int c=0;
 int n=sc.nextInt();
 ArrayList<Integer> al=new ArrayList<Integer>();
 for(int i=0;i<n;i++){
@@ -13,7 +14,14 @@ a=fun(al);
 System.out.println(a);
 }
 public static ArrayList<Integer> fun(ArrayList<Integer> a){
-
+ArrayList<Integer> ar=new ArrayList<Integer>();
+if(c!=n){
+ c++;
+ ar=fun(a);
+}
+ar.add(a.get(c));
+c--;
+return ar;
 }
 
 
