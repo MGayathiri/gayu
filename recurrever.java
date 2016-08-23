f4abeb50016cde3af9743668e0d102b20ee498a5
MGayathiri <gayathiricse14@gmail.com>
2016-08-23 09:00:59 +0530

    Create recurrever.java

---
+++ recurrever.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import jva.util.*:
+class recurrever{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+ArrayList<Integer> al=new ArrayList<Integer>();
+for(int i=0;i<n;i++){
+al.add(sc.nextInt());
+ }
+ArrayList<Integer> a=new ArrayList<Integer>()
+a=fun(al);
+System.out.println(a);
+}
+public static ArrayList<Integer> fun(ArrayList<Integer> a){
+
+}
+
+
+}
