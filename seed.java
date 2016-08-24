48612f93aa99c007da19979a1edf8cee3751628b
MGayathiri <gayathiricse14@gmail.com>
2016-08-24 08:55:42 +0530

    Update seed.java

--- seed.java
+++ seed.java
@@ -4,6 +4,19 @@ class seed{
 public static void main(String[] args){
 Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
-for(int )
+if(n>0){
+for(int i=1;i<n;i++ ){
+  int m=1;
+  m=m*i;
+  int x=i;
+  while(x!=0){
+    m=m*(x%10);
+    x=x/10;
+  }
+  if(m==n){System.out.println(i);}
+  
+ }
+   }
+
 }
 }
