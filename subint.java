b82beba22327ac1966e4f36040bcc169f5d12713
MGayathiri <gayathiricse14@gmail.com>
2016-08-25 09:19:45 +0530

    Create subint.java

---
+++ subint.java
@@ -0,0 +1,20 @@
+import java.io.*:
+import java.util.*;
+class subint{
+public static void main(Strinig[] args){
+Scanner sc= new Scanner(System.in);
+int n=sc.nextInt();
+int m=sc.nextInt();
+if(n<m){
+itn c=0;
+List<Integer> a=new ArrayList<Integer>();
+for(int i=0;i<n;i++){
+ a[i]=sc.nextint();
+ }
+for(int i=0;i<m;i++){
+  int x=sc.nextInt();
+  if()
+  }
+}
+}
+}
