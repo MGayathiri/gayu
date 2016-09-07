f244e883faed8933451af3896f418288a7505a54
MGayathiri <gayathiricse14@gmail.com>
2016-09-07 14:05:44 +0900

    Update pangram.java

--- pangram.java
+++ pangram.java
@@ -4,10 +4,10 @@ class pangram{
 public static void main(String[] args){
 Scanner sc=new Scanner(System.in);
 String s=sc.next();
-Set<String> st=TreeSet<String>();
+Set<Character> st=new TreeSet<Character>();
 int i=0;
 for(i=0;i<s.length();i++){
-if(Character.isLatter(s.charAt(i)){
+if(Character.isLetter(s.charAt(i)){
 st.add(s.charAt(i));}
 else{break;}
 }
